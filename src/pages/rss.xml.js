0fdc3eef7b6349be60fc2206f3bb4ff3cf9f8c55
GhostDealerSJr <ivmendozagil@gmail.com>
2025-07-28 23:49:32 -0600

    update page

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,8 +2,8 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'SofiDev | Links',
-    description: 'All my Links',
+    title: 'Ghost Dealer SJR | Links',
+    description: 'Links Oficiales',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>es-mx</language>`,
